cab755766218bee4b334336c50966f2a36d93699
ShmPt <cacahouettetbs@gmail.com>
2024-05-15 10:51:46 +0200

    feat: complete fifth conditional branching exercice

---
+++ 2.10_conditionalBranching/conditionalOperator2.js
@@ -0,0 +1,24 @@
+//Conditional branching
+
+//Rewrite if into ?
+
+//Rewrite this if else using multiple ternary operators '?':
+//For readability, it's recommended to split the code into multiple lines.
+/*
+let message;
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
+*/
+
+let message = (login == "Employee") ? 'Hello' : 
+            (login == "Director") ? 'Greetings' :
+            (login == '') ? "No login" : 
+            "";
