89f53df5f0a4707a395a50f77ab7fe798817e733
ShmPt <cacahouettetbs@gmail.com>
2024-05-15 15:52:37 +0200

    feat: added arrow function exercise

---
+++ 2.17_arrowFunctions/arrowFunctions.js
@@ -0,0 +1,30 @@
+//Arrow Functions
+
+//Rewrite with arrow functions
+
+//Replace Function Expressions with arrow functions in the code below:
+/*
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  function() { alert("You agreed."); },
+  function() { alert("You canceled the execution."); }
+);
+*/
+
+"use strict"
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes();
+    else no();
+  }
+  
+  ask(
+    "Do you agree?",
+    () => alert("You agreed."),
+    () => alert("You canceled the execution.")
+  );
