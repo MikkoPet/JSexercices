464dcddeb76c5fa12fc18a2497a038e7854ebce2
ShmPt <cacahouettetbs@gmail.com>
2024-05-15 12:08:58 +0200

    feat: complete second set of exercises on logical operators

---
+++ 2.11_logicalOperators/checkRange.js
@@ -0,0 +1,26 @@
+//Logical Operators
+
+//Check the range between
+
+//Write an if condition to check that age is between 14 and 90 inclusively.
+//"Inclusively” means that age can reach the edges 14 or 90.
+
+"use strict"
+
+let age;
+
+if (14 <= age && age <= 90){
+
+}
+
+//Write an if condition to check that age is NOT between 14 and 90 inclusively.
+//Create two variants: the first one using NOT !, the second one – without it.
+
+if (!(14 <= age && age <= 90)){
+
+}
+
+if (14 > age || age > 90){
+
+}
+
