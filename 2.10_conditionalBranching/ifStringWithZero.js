913403f46c5d05b640c1ead26ddd662b193df061
ShmPt <cacahouettetbs@gmail.com>
2024-05-15 10:32:19 +0200

    feat: complete first conditional exercice

---
+++ 2.10_conditionalBranching/ifStringWithZero.js
@@ -0,0 +1,18 @@
+//Conditional branching
+
+//if (a string with zero)
+
+//Will alert be shown?
+
+"use strict"
+
+if ("0") {
+    alert( 'Hello' );
+  }
+
+  // alert will be shown.
+  // the condition of if is converted to a boolean
+  // "0" is a string
+  // a String containing any amount of any character returns true in boolean
+  // so the code within the if exercutes
+  // hence the alert appears.
