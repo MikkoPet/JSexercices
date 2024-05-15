0574a6900962c042d193d0e59a4268345e334226
ShmPt <cacahouettetbs@gmail.com>
2024-05-15 09:48:16 +0200

    test: complete first exercice of the operators page

---
+++ 2.8_Basic Operators Maths/postfixAndPrefixForms.js
@@ -0,0 +1,12 @@
+//Basic operators, maths
+
+//The postfix and prefix form
+
+//What are the final values of all variables a, b, c and d, in the code below?
+
+"use strict"
+
+let a = 1, b = 1;
+
+let c = ++a; // a = 2, c = 2
+let d = b++; // d = 1, b = 2
